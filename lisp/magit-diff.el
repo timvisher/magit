908cc1160fa6e282bd6a4c0deda420da2f33b942
Jonas Bernoulli <jonas@bernoul.li>
2016-09-29 22:38:00 +0200

    magit-diff-highlight-hunk-region-using-underline: new variant

--- lisp/magit-diff.el
+++ lisp/magit-diff.el
@@ -114,7 +114,9 @@ instead customize `magit-diff-highlight-hunk-region-functions'."
 (defcustom magit-diff-highlight-hunk-region-functions
   `(magit-diff-highlight-hunk-region-dim-outside
     ,@(and magit-diff-show-lines-boundary
-           (list 'magit-diff-highlight-hunk-region-using-overlays)))
+           (list (if (version< emacs-version "25.1")
+                     'magit-diff-highlight-hunk-region-using-overlays
+                   'magit-diff-highlight-hunk-region-using-underline))))
   "The functions used to highlight the hunk-internal region.
 
 `magit-diff-highlight-hunk-region-dim-outside' overlays the outside
@@ -122,8 +124,11 @@ of the hunk internal selection with a face that causes the added and
 removed lines to have the same background color as context lines.
 This function should not be removed from the value of this option.
 
-`magit-diff-highlight-hunk-region-using-overlays' emphasize the
-region by placing delimiting horizonal lines before and after it."
+`magit-diff-highlight-hunk-region-using-overlays' and
+`magit-diff-highlight-hunk-region-using-underline' emphasize the
+region by placing delimiting horizonal lines before and after it.
+Which implementation is preferable depends on the Emacs version,
+and the more suitable one is part of the default value."
   :package-version '(magit . "2.9.0")
   :set-after '(magit-diff-show-lines-boundaries)
   :group 'magit-diff
@@ -2159,6 +2164,27 @@ are highlighted."
                   'face 'magit-diff-lines-boundary)))
         (magit-diff--make-hunk-overlay beg (1+ beg) 'before-string str)
         (magit-diff--make-hunk-overlay end (1+ end) 'after-string  str))
+    ))
+
+(defun magit-diff-highlight-hunk-region-using-underline (section)
+  (if (window-system)
+      (let* ((beg (magit-diff-hunk-region-beginning))
+             (end (magit-diff-hunk-region-end))
+             (beg-eol (save-excursion (goto-char beg)
+                                      (end-of-visual-line)
+                                      (point)))
+             (end-bol (save-excursion (goto-char end)
+                                      (beginning-of-visual-line)
+                                      (point)))
+             (color (face-background 'magit-diff-lines-boundary nil t)))
+        (cl-flet ((ln (b e &rest face)
+                      (magit-diff--make-hunk-overlay
+                       b e 'face face 'after-string
+                       (magit-diff--hunk-after-string face))))
+          (if (= beg end-bol)
+              (ln beg beg-eol :overline color :underline color)
+            (ln beg beg-eol :overline color)
+            (ln end-bol end :underline color))))
     ))
 
 (defun magit-diff--make-hunk-overlay (start end &rest args)
